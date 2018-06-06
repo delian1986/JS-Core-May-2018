f4e74561f45201368270ed7ca45ed5e58832b6b2
delian1986 <delian1914@gmail.com>
2018-06-06 22:22:46 +0300

    Objects and Maps homework

---
+++ JS Fundamentals/Exams/exam 11 Feb 2018/04. Game of Epicness.js
@@ -0,0 +1,21 @@
+function gameOfEpicness(army,battle) {
+
+    let armies=new Map();
+
+    for (let i = 0; i < army.length; i++) {
+
+    }
+}
+
+gameOfEpicness([ { kingdom: "Maiden Way", general: "Merek", army: 5000 },
+        { kingdom: "Stonegate", general: "Ulric", army: 4900 },
+        { kingdom: "Stonegate", general: "Doran", army: 70000 },
+        { kingdom: "YorkenShire", general: "Quinn", army: 0 },
+        { kingdom: "YorkenShire", general: "Quinn", army: 2000 },
+        { kingdom: "Maiden Way", general: "Berinon", army: 100000 } ],
+    [ ["YorkenShire", "Quinn", "Stonegate", "Ulric"],
+        ["Stonegate", "Ulric", "Stonegate", "Doran"],
+        ["Stonegate", "Doran", "Maiden Way", "Merek"],
+        ["Stonegate", "Ulric", "Maiden Way", "Merek"],
+        ["Maiden Way", "Berinon", "Stonegate", "Ulric"] ]
+);
