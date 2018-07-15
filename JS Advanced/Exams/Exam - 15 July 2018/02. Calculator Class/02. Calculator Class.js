6d72e8497086e4e4dbd97dbb46fbdf6cc45489db
delian1986 <delian1914@gmail.com>
2018-07-15 20:18:50 +0300

    Testing corrected

--- JS Advanced/Exams/Exam - 15 July 2018/02. Calculator Class/02. Calculator Class.js
+++ JS Advanced/Exams/Exam - 15 July 2018/02. Calculator Class/02. Calculator Class.js
@@ -56,13 +56,15 @@ class Calculator {
 }
 
 //90/100
+let calc;
+beforeEach(function () {
+    calc = new Calculator();
+});
 describe('', function () {
     it('empty', function () {
-        let calc = new Calculator();
         expect(calc.toString()).to.equal('empty array')
     });
     it('add', function () {
-        let calc = new Calculator();
         calc.add(2);
         calc.add(4);
         calc.add('pesho');
@@ -70,7 +72,6 @@ describe('', function () {
         expect(calc.toString()).to.equal('2 -> 4 -> pesho -> [object Object]')
     });
     it('divide', function () {
-        let calc = new Calculator();
         calc.add(2);
         calc.add(4);
         calc.add('pesho');
@@ -78,60 +79,50 @@ describe('', function () {
         expect(calc.divideNums()).to.equal(0.5)
     });
     it('divide empty', function () {
-        let calc = new Calculator();
         expect(calc.toString()).to.equal('empty array')
     });
     it('divide by one num', function () {
-        let calc = new Calculator();
         calc.add(2);
         expect(calc.divideNums()).to.equal(2)
     });
     it('divide by zero', function () {
-        let calc = new Calculator();
         calc.add(2);
         calc.add(0);
         expect(calc.divideNums()).to.equal("Cannot divide by zero")
     });
     it('divide by negative', function () {
-        let calc = new Calculator();
         calc.add(2);
         calc.add(-11);
         expect(calc.divideNums()).to.equal(-0.18181818181818182)
     });
     it('divide only negative', function () {
-        let calc = new Calculator();
         calc.add(-12);
         calc.add(-11);
         expect(calc.divideNums()).to.equal(1.0909090909090908)
     });
     it('divide floats', function () {
-        let calc = new Calculator();
         calc.add(-12.1);
         calc.add(-11.22);
         expect(calc.divideNums()).to.equal(1.0784313725490196)
     });
     it('divide more than two', function () {
-        let calc = new Calculator();
         calc.add(-12);
         calc.add(12);
         calc.add(-11);
         expect(calc.divideNums()).to.equal(0.09090909090909091)
     });
     it('divide with no nums', function () {
-        let calc = new Calculator();
         calc.add('wdas');
         calc.add({});
         calc.add('sda');
         expect(() => calc.divideNums().to.throw('There are no numbers in the array!'))
     });
     it('orderby nums', function () {
-        let calc = new Calculator();
         calc.add(12);
         calc.add(4);
         expect(calc.orderBy()).to.equal("4, 12")
     });
     it('orderby mixed', function () {
-        let calc = new Calculator();
         calc.add(12);
         calc.add(-4);
         calc.add(0);
@@ -140,18 +131,15 @@ describe('', function () {
         expect(calc.orderBy()).to.equal("-4, 0, 12, [object Object], pesh")
     });
     it('orderby empty', function () {
-        let calc = new Calculator();
         expect(calc.orderBy()).to.equal("empty")
     });
     it('orderby non nums', function () {
-        let calc = new Calculator();
         calc.add('sad');
         calc.add({});
         calc.add('2');
         expect(calc.orderBy()).to.equal("2, [object Object], sad")
     });
     it('one elelemt arr', function () {
-        let calc = new Calculator();
         calc.add(-1);
         expect(calc.toString()).to.equal('-1')
     });
