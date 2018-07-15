5602699f15bc568608595e106c3a3890a3358e27
delian1986 <delian1914@gmail.com>
2018-07-15 19:40:30 +0300

    JS Classes homework solved

---
+++ JS Advanced/06. JS Classes/Exercises/08. View Model.js
@@ -0,0 +1,30 @@
+class Textbox {
+    constructor (selector,regex){
+        this.selector=selector;
+        this._regex=regex;
+        this._elements=$(this.selector);
+        $(this.selector).on('input',function () {
+            $('*[type=text]').val(this.value);
+        })
+    }
+
+    get value(){
+        return this.elements.val();
+    }
+    set value(newValue){
+        this.elements.val(newValue)
+    }
+
+    get elements(){
+        return this._elements;
+    }
+    isValid(){
+        return !this.value.match(this._regex)
+    }
+
+}
+
+let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
+let inputs = $('.textbox');
+
+inputs.on('input',function(){console.log(textbox.value);});
