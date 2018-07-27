b7c1eaedd48d692de089b4d82ab8153fbff2570a
delian1986 <delian1914@gmail.com>
2018-07-27 22:26:29 +0300

    Venue task solved

--- JS Apps/04. Async Programming/06. Venue Master/venuemaster.js
+++ JS Apps/04. Async Programming/06. Venue Master/venuemaster.js
@@ -1,10 +1,12 @@
+//this code need rework
+
+
 function attachEvents() {
     $('#getVenues').on('click', getVenues);
     const header = {'Authorization': 'Basic Z3Vlc3Q6cGFzcw=='};
 
     function getVenues() {
         let date = $('#venueDate').val();
-        console.log(date);
         const postDateUrl = `https://baas.kinvey.com/rpc/kid_BJ_Ke8hZg/custom/calendar?query=${date}`;
         $.ajax({
             method: 'POST',
@@ -15,6 +17,7 @@ function attachEvents() {
 
     function getThisDayVenues(response) {
         const getVenueUrl = 'https://baas.kinvey.com/appdata/kid_BJ_Ke8hZg/venues/';
+        $('#venue-info').empty();
         for (let event of response) {
             $.ajax({
                 method: 'GET',
@@ -32,7 +35,7 @@ function attachEvents() {
         table.append(`<tr><th>Ticket Price</th><th>Quantity</th><th></th></tr>`)
             .append($('<tr>'))
             .append($(`<td class="venue-price">${venue.price} lv</td>`));
-        let td=($('<td>'));
+        let td = ($('<td>'));
         let select = ($(`<select class="quantity">`))
             .append($('<option value="1">1</option>'))
             .append($('<option value="2">2</option>'))
@@ -44,17 +47,51 @@ function attachEvents() {
         table.append($('<td><input class="purchase" type="button" value="Purchase"></td>'));
 
 
-        let descriptionSection=$('<span class="head">Venue description:</span>');
+        let descriptionSection = $('<span class="head">Venue description:</span>');
         descriptionSection.append($(`<p class="description">${venue.description}</p>`))
             .append($(`<p class="description">Starting time: ${venue.startingHour}</p>`));
         venueDetails.append(table);
         venueDetails.append(descriptionSection);
         divBlock.append(venueDetails);
+
         $('#venue-info').append(divBlock);
-
-        $('#moreInfo').on('')
-
+        $('.purchase').on('click', purchase);
+        $('.info').on('click', info)
     }
+
+    function info() {
+        $('.venue-details').hide();
+        $(this).parent().parent().find('.venue-details').show();
+    }
+
+    function purchase() {
+        console.log($(this));
+        let id = $(this).parent().parent().parent().parent().parent().attr('id');
+        let name = $(this).parent().parent().parent().parent().parent().find(".venue-name").text();
+        let qty = Number($(this).parent().parent().find(".quantity").val());
+        let price = Number($(this).parent().parent().find(".venue-price").text().substring(0, $(this).parent().parent().find(".venue-price").text().length - 2));
+
+        $('#venue-info').html(`<span class="head">Confirm purchase</span>
+<div class="purchase-info">
+  <span>${name}</span>
+  <span>${qty} x ${price}</span>
+  <span>Total: ${qty * price} lv</span>
+  <input type="button" value="Confirm">
+</div>`);
+
+        $('#venue-info input').click(function () {
+            let request = {
+                method: "POST",
+                url: `https://baas.kinvey.com/rpc/kid_BJ_Ke8hZg/custom/purchase?venue=${id}&qty=${qty}`,
+                headers: header
+            };
+
+            $.ajax(request).then(function (data) {
+                $('#venue-info').html("You may print this page as your ticket" + data.html);
+            })
+        })
+    }
+
 
     function displayError(err) {
         $('#venue-info').append(err.statusText)
