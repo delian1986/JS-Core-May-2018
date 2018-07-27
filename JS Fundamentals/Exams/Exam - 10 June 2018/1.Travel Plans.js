51cc285447542741e5a4148a138643394aaa2fbd
delian1986 <delian1914@gmail.com>
2018-07-27 22:29:19 +0300

    added some code to 10 june exam... still not solved

--- JS Fundamentals/Exams/Exam - 10 June 2018/1.Travel Plans.js
+++ JS Fundamentals/Exams/Exam - 10 June 2018/1.Travel Plans.js
@@ -4,15 +4,28 @@ function travelPlans(inputArr) {
         average: ['Driving', 'Managing', 'Fishing', 'Gardening'],
         clumsy:['Singing', 'Accounting', 'Teaching', 'Exam-Making', 'Acting', 'Writing', 'Lecturing', 'Modeling', 'Nursing']
     };
+    let specializedCount=0;
+    let averageCount=0;
+    let clumsyCount=0;
+    let totalGold=0;
 
-    for (let i = 0; i < inputArr.length; i++) {
-        let args=inputArr[i].split(/\W+/);
-        for (let mariykaElement in mariyka) {
-            for (let value of mariyka[mariykaElement]) {
-                console.log(value);
-            }
+    for (let line of inputArr) {
+        let [spec,gold]=line.split(' : ');
+        totalGold+=Number(gold);
+
+        if (mariyka.specialized.includes(spec)){
+            specializedCount++;
+        }else if(mariyka.average.includes(spec)){
+            averageCount++
+        }else if(mariyka.clumsy.includes(spec)){
+            clumsyCount++;
         }
     }
+
+    console.log(specializedCount);
+    console.log(averageCount);
+    console.log(clumsyCount);
+    console.log(totalGold);
 }
 
 // travelPlans(["Programming : 500", "Driving : 243.55", "Acting : 200", "Singing : 100", "Cooking : 199’, "Hardware maintenance : 800", "Gardening : 700", "Programming : 500"]);
