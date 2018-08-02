5378f1c76c6649c1025122f2bd36b8b36d821262
delian1986 <delian1914@gmail.com>
2018-08-02 22:45:44 +0300

    Hello Handlebars

--- JS Apps/06. Templating/Hello Handlebars/hello.js
+++ JS Apps/06. Templating/Hello Handlebars/hello.js
@@ -1,4 +1,13 @@
-let template=async()=> {
-    return await $.get('/template.hbs')
+let data={
+    title:'I am cool title',
+    body:'I am cool body'
 };
+
+(async function getTemplate() {
+   let temp=await $.get('template.hbs');
+
+    let list=Handlebars.compile(temp);
+    $('#content').html(list(data))
+})();
+
 
